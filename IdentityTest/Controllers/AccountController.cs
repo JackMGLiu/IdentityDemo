80245ad345c627fac17a099cd4b6b13c412c2d63
Jack-Liu <love_zs1314@126.com>
2018-08-27 17:37:14 +0800

    验证

---
+++ IdentityTest/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityTest.Controllers
+{
+    public class AccountController : Controller
+    {
+        public async Task<IActionResult> Login(string returnUrl)
+        {
+            //if (!string.IsNullOrEmpty(returnUrl))
+            //{
+            //    return Redirect(returnUrl);
+            //}
+            return View();
+        }
+
+        public async Task<IActionResult> Denied()
+        {
+            return View();
+        }
+
+        #region 模拟登录
+
+        public async Task<IActionResult> MakeLogin()
+        {
+            var claims = new List<Claim>(){
+                new Claim(ClaimTypes.Name,"liujian"),
+                new Claim(ClaimTypes.Role,"admin")
+            };
+
+            //必须要加CookieAuthenticationDefaults.AuthenticationScheme，不然无法解析
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+            return Ok();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Ok();
+        }
+
+
+        #endregion
+    }
+}
---
+++ IdentityTest/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityTest
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/Account/Login";
+                    options.AccessDeniedPath = "/Account/Denied";
+                    //options.AccessDeniedPath = new PathString("/Account/Denied");
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.Map("/task", t =>
+            //{
+            //    t.Run(async (context) =>
+            //    {
+            //        await context.Response.WriteAsync("this is a task！");
+            //    });
+            //});
+
+            app.UseAuthentication();
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("1：ext first！");
+            //    await next.Invoke();
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("2：ext second！");
+            //    await next.Invoke();
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("3：ext third！");
+            //});
+        }
+    }
+}
