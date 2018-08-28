ab350ed71a24d07215754c2d3629bac8c428e78b
Jack-Liu <love_zs1314@126.com>
2018-08-28 15:48:18 +0800

    有问题未处理

--- Id4Server/Startup.cs
+++ Id4Server/Startup.cs
@@ -3,18 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Id4Server.Data;
+using Id4Server.Models;
+using Id4Server.Services;
 
 namespace Id4Server
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            /*
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+            */
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()//添加开发人员签名凭据
+                .AddInMemoryApiResources(Config.GetApiResources())//添加内存apiresource
+                .AddInMemoryClients(Config.GetClients())//添加内存client
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())//添加系统中的资源
+                .AddTestUsers(Config.GetTestUsers());//添加测试用户
+
             services.AddMvc();
         }
 
@@ -23,8 +52,27 @@ namespace Id4Server
         {
             if (env.IsDevelopment())
             {
+                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            //app.UseAuthentication();
+
+            app.UseIdentityServer();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
